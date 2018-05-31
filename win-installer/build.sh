b205bece64364e3f283c78bcda10b09cd9a0491a
sherzod <sherzod.gapirov@mail.ru>
2018-05-31 17:13:24 +0500

    Do test win-installer

--- win-installer/build.sh
+++ win-installer/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 set -e
-set -c
+set -v
 
-wine /home/wix/wix/heat.exe dir stage -t set-platform.xslt -gg -cg PravdaCLIComponent -ke -srd -dr PravdaCLI -sfrag -o PravdaCLI.wxs
-wine /home/wix/wix/candle.exe PravdaCLI.wxs
-wine /home/wix/wix/candle.exe pravda.wxs
-wine /home/wix/wix/light.exe -b pravda PravdaCLI.wixobj pravda.wixobj -sval -o pravda.msi
+export WINEPREFIX=/root/.wine
+
+wine /wix/heat.exe dir stage -t set-platform.xslt -gg -cg PravdaCLIComponent -ke -srd -dr PravdaCLI -sfrag -o PravdaCLI.wxs
+wine /wix/candle.exe PravdaCLI.wxs
+wine /wix/candle.exe pravda.wxs
+wine /wix/light.exe -b stage PravdaCLI.wixobj pravda.wixobj -sval -o pravda.msi
 
